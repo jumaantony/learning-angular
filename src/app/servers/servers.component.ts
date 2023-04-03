b7f28d63c1d78cb18618b547e33a911e6790b5ef
Juma Antony <jumaantony840@gmail.com>
2023-04-04 00:33:33 +0300

    styling angular components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  // styleUrls: ['./servers.component.css']
+  styles: [`
+    p { color: dodgerblue; }
+  `]
 })
 export class ServersComponent {
 
